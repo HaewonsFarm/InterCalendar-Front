bfc6cb46d52ba8a00604851aea9efc344c003fec
Haewon <155549040+HaewonsFarm@users.noreply.github.com>
2024-07-12 22:28:25 +0900

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSessionReducer from "./user/userSessionSlice";
 import userInfoReducer from "./user/userInfoSlice";
+import calendarReducer from "../redux/reducers/calendarReducer";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     userSession: userSessionReducer,
     userInfo: userInfoReducer,
+    calendar: calendarReducer,
   },
 });
+
+export default store;
