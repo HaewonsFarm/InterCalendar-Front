1bfe12d44771c6788b06f87cb5de30b89b3cc35f
Haewon <155549040+HaewonsFarm@users.noreply.github.com>
2024-07-12 22:34:45 +0900

    Update MainPage.jsx
    What's changed: Scheduler has been added with a toggle button. Also, if there is no data, an empty array is passed, making it a empty calendar.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,22 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Header from "../components/Header";
 import SideBar from "../components/SideBar";
-import Demo from "../components/Calendar";
-
+import Calendar from "../components/Calendar";
+import SchedulerComponent from "../components/Scheduler";
+import { fetchEvents } from '../redux/actions/calendarActions';
 import "../styles/pages/MainPage.scss";
+// import todayAppointments from '../demo-data/today-appointment';
+// ^ 백엔드 대신
 
 const MainPage = () => {
+  const dispatch = useDispatch();
+  // 스토어의 상태 받아오기
+  const { events, loading, error } = useSelector((state) => state.calendar);
+  // 토글 버튼 만드는 데 들어가는 State
+  const [showCalendar, setShowCalendar] = useState(true);
+  // 컴포넌트가 마운트 될 때 이벤트를 받아옴.
+  useEffect(() => {
+    dispatch(fetchEvents());
+  }, [dispatch]);
+
+  const toggleView = () => {
+    setShowCalendar(prevShowCalendar => !prevShowCalendar);
+  };
+
+  const eventList = Array.isArray(events) && events.length > 0 ? events : [];
+  // && events.length > 0 ? events : todayAppointments`
+  // ^ 백엔드 대신
+
   return (
-    <>
-      <div className="scaffold">
-        <SideBar />
-        <div className="main-body">
-          <Header />
+    <div className="scaffold">
+      <SideBar />
+      <div className="main-body">
+        <Header />
+        <div className="main-container">
+          <div className="button-container">
+          <button 
+            className="toggle-button"
+            onClick={toggleView}
+          >
+            {showCalendar ? 'Show Scheduler' : 'Show Calendar'}
+          </button>
+          </div>
+          {loading && <p>Loading...</p>}
+          {error && <p>Error: {error}</p>}
           <div className="main-calendar">
-          <Demo />
+          {showCalendar ? (
+            <Calendar events={eventList} />
+            ) : (
+            <SchedulerComponent events={eventList} />
+          )}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
