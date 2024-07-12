84490fa7489dc7758b97d9d277d0232bef22af3b
Haewon <155549040+HaewonsFarm@users.noreply.github.com>
2024-07-12 22:50:04 +0900

    Update calendarActions.js

--- src/redux/actions/calendarActions.js
+++ src/redux/actions/calendarActions.js
@@ -7,7 +7,7 @@ export const FETCH_EVENTS_FAILURE = 'FETCH_EVENTS_FAILURE';
 export const fetchEvents = () => async (dispatch) => {
   dispatch({ type: FETCH_EVENTS_REQUEST });
   try {
-    const response = await axios.get('/api/calendar'); // 백엔드 엔드포인트에 맞게 수정
+    const response = await axios.get('/api/calendar'); // 백엔드 엔드포인트
     dispatch({
       type: FETCH_EVENTS_SUCCESS,
       payload: response.data,
