ec59c3354d09f017381dbd738188d657e035398d
Haewon <155549040+HaewonsFarm@users.noreply.github.com>
2024-07-13 21:30:13 +0900

    Update store.js
    groupPage, itemPage의 Slice 추가

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSessionReducer from "./user/userSessionSlice";
 import userInfoReducer from "./user/userInfoSlice";
 import calendarReducer from "../redux/reducers/calendarReducer";
+import groupReducer from "../redux/groupSlice";
+import itemReducer from "../redux/itemSlice";
 
 const store = configureStore({
   reducer: {
     userSession: userSessionReducer,
     userInfo: userInfoReducer,
     calendar: calendarReducer,
+    group: groupReducer,
+    item: itemReducer,
   },
 });
 
